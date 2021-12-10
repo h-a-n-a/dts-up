1e547011a28e17ea06743238bef7489edae3fc4b
wangshuyuan.h <wangshuyuan.h@bytedance.com>
2021-12-10 21:54:29 +0800

    feat: add basic impl

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "extends": "@hanao/eslint-config-basic"
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import ts from 'typescript'
+
+interface Options {
+  entryPoints: string[]
+}
+
+const dts = () => {
+  console.log(123)
+}
+
+export = dts
